3202c145e160787ba5e5ffed406c043fefe49a48
sw <1049787469@qq.com>
2017-04-26 14:34:06 +0800

    new function

--- app/views.py
+++ app/views.py
@@ -1,9 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import os
 from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.conf import settings
+from statistics.stat_data import StatData
+
 
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request, 'app/upload.html')
+
+def cal(request):
+    dt=StatData('/home/sw/stats/upload')
+    rs=dt.get_tr_rs()
+    return render(request, 'app/detail.html',{'rs_map':rs})
+def upload_file(request):
+    if request.method == "POST": 
+        file_list =request.FILES.getlist("myfile")
+        if not file_list:  
+            return HttpResponse("no files for upload!")
+        for f in file_list:
+            destination = open(os.path.join(settings.UPLOAD_PATH,f.name),'wb+')
+            for chunk in f.chunks():      # 分块写入文件  
+                destination.write(chunk)  
+            destination.close()  
+        return render(request,'app/success.html')
--- statistics/data_source.py
+++ statistics/data_source.py
@@ -1,24 +1,61 @@
 #coding:utf-8
+import os
+from django.conf import settings
 from pyexcel_xls import get_data,save_data
 from collections import OrderedDict
-from statistics.models import TianRong
+from statistics.models import TianRong,QingNiu,GuDe
 
 class DataSource(object):
     
-    def __init__(self,fpath):
-        self.fpath=fpath
-        self.origin_data = []
+    def __init__(self,dir_path):
+        self.dir_path = os.listdir(dir_path)
+        self.tianrong_rs = []
+        self.qingniu_rs = []
+        self.gude_rs = []
         self.load_data()
-        self.rs=[]
+
+    def get_xlx_data(self,fpath):
+        return get_data(fpath)
 
     def load_data(self):
-        rs = self.fpath and get_data(self.fpath) or {}
-        self.origin_data = rs and rs['Sheet1'] or []
-    def get_json(self):
-        for it in self.origin_data[1:100]:
+        for it in self.dir_path:
+            if u'青牛' in it:
+                print '>>>>>>>>',it
+                self.get_qingniu(settings.UPLOAD_PATH+'/'+it)
+            elif u'古德' in it:
+                print 'zzzzzzzzzz',it
+                self.get_gude(settings.UPLOAD_PATH+'/'+it)
+            elif u'天润' in it:
+                print 'xxxxxxxxxxx',it
+                self.get_tianrong(settings.UPLOAD_PATH+'/'+it)
+    def get_tianrong(self,fpath):
+        data=get_data(fpath)
+        rs = data and data['Sheet1'] or []
+        for it in rs[1:]:
             tr=TianRong(it)
-            self.rs.append(tr)
-        return self.rs
+            self.tianrong_rs.append(tr)
+        return self.tianrong_rs
+
+    def get_qingniu(self,fpath):
+        data=get_data(fpath)
+        rs = data and data['Sheet1'] or []
+        for it in rs[4:]:
+            tr=QingNiu(it)
+            self.qingniu_rs.append(tr)
+        return self.qingniu_rs
+
+    def get_gude(self,fpath):
+        data=get_data(fpath)
+        rs = data and data['Sheet1'] or []
+        for it in rs[1:]:
+            tr=GuDe(it)
+            self.gude_rs.append(tr)
+        return self.gude_rs
+
+
+
+
+
+
 if __name__=='__main__':
-    dt=DataSource('天润系统.xlsx')
-    print dt.get_json()
+    dt=DataSource('/home/sw/stats/upload')
