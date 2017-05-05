89315a213e5a1d7c23e324f051720a9006148032
sw <1049787469@qq.com>
2017-05-05 09:07:38 +0800

    exe

--- statistics/data_source.py
+++ statistics/data_source.py
@@ -3,19 +3,21 @@ import os
 from django.conf import settings
 from pyexcel_xls import get_data,save_data
 from collections import OrderedDict
-from statistics.models import TianRong,QingNiu,GuDe
+#from statistics.models import TianRong,QingNiu,GuDe
+from models import TianRong,QingNiu,GuDe
 
 class DataSource(object):
     
-    def __init__(self,dir_path):
-        self.dir_path = os.listdir(dir_path)
+    def __init__(self,file_list):
+        #self.dir_path = os.listdir(dir_path)
+        self.filename_list = file_list
         self.tianrong_rs = []
         self.qingniu_rs = []
         self.gude_rs = []
         self.load_data()
 
     def get_expt_phone_map(self):
-        f=open('conf/phone.txt','r')
+        f=open('../conf/phone.txt','r')
         lines=f.readlines()
         f.close()
         #mulsub=lambda x,i,j,s:x[:i] + ''.join(map( lambda it:it.replace(it,s),x[i:j] )) + x[j:]
@@ -26,16 +28,20 @@ class DataSource(object):
         return get_data(fpath)
 
     def load_data(self):
-        for it in self.dir_path:
+        #for it in self.dir_path:
+        for it in self.filename_list:
             if u'青牛' in it:
                 print '>>>>>>>>',it
-                self.get_qingniu(settings.UPLOAD_PATH+'/'+it)
+                #self.get_qingniu(settings.UPLOAD_PATH+'/'+it)
+                self.get_qingniu(it)
             elif u'古德' in it:
                 print 'zzzzzzzzzz',it
-                self.get_gude(settings.UPLOAD_PATH+'/'+it)
+                #self.get_gude(settings.UPLOAD_PATH+'/'+it)
+                self.get_gude(it)
             elif u'天润' in it:
                 print 'xxxxxxxxxxx',it
-                self.get_tianrong(settings.UPLOAD_PATH+'/'+it)
+                #self.get_tianrong(settings.UPLOAD_PATH+'/'+it)
+                self.get_tianrong(it)
     def get_tianrong(self,fpath):
         data=get_data(fpath)
         expt_mp=self.get_expt_phone_map()
@@ -70,3 +76,4 @@ class DataSource(object):
 
 if __name__=='__main__':
     dt=DataSource('/home/sw/stats/upload')
+    
